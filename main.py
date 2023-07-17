6613453b16c89d2382a03dd352964829954b383e
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-17 14:52:42 +0200

    another changes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-github_user = input('Input Github User: ') #prosimy uzytkownika o podanie nazwy profilu github
+# github_user = input('Input Github User: ') #prosimy uzytkownika o podanie nazwy profilu github
+url = 'https://github.com/abrakadabror?tab=repositories'
 
-url = 'https://github.com/' + github_user #tworzony jest adres URL na podstawie ww. przekazanej nazwy
+# url = 'https://github.com/' + github_user #tworzony jest adres URL na podstawie ww. przekazanej nazwy
 response = requests.get(url) 
 if response.status_code == 200:
     print("Everything's gonna be alright")
@@ -13,8 +14,17 @@ else:
     print('Give me a patience to live in this place...an error occured!')
 
 soup = BeautifulSoup(response.content, 'html.parser')
-profile_image = soup.find('img', {'alt': 'Avatar'})['src']
-print(profile_image)
+# profile_image = soup.find('img', {'alt': 'Avatar'})['src']
+# print(profile_image)
 
-
+# logo_github  = soup.find('svg', class_ =  'octicon-mark-github')
+# logo_src = logo_github.get('src')
+# print(logo_src)
 # program do wydrukowania linku do avatara uztkownika github
+
+repositories = soup.find_all(class_ = 'd-inline-block mb-1')
+repository_list = []
+for repository in repositories:
+    repository_name = repository.text.strip()
+    print(repository_name)
+print(repository_list)

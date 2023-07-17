91f587ed16ee60ea458a28ab4b4763fabe3c80c7
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-17 13:35:21 +0200

    # program do wydrukowania linku do avatara uztkownika github

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-github_user = input('Input Github User: ')
+github_user = input('Input Github User: ') #prosimy uzytkownika o podanie nazwy profilu github
 
-url = 'https://github.com/' + github_user
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+url = 'https://github.com/' + github_user #tworzony jest adres URL na podstawie ww. przekazanej nazwy
+response = requests.get(url) 
+if response.status_code == 200:
+    print("Everything's gonna be alright")
+elif response.status_code == 404:
+    print("Fatall error - it's a faulty operation")
+else:
+    print('Give me a patience to live in this place...an error occured!')
+
+soup = BeautifulSoup(response.content, 'html.parser')
 profile_image = soup.find('img', {'alt': 'Avatar'})['src']
 print(profile_image)
 

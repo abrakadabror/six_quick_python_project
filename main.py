d6a3e0d06e0bea993b60bcddd93d94d067fb66db
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-18 10:58:29 +0200

    weather_data

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
+from pprint import pprint
 
-# github_user = input('Input Github User: ') #prosimy uzytkownika o podanie nazwy profilu github
-url = 'https://github.com/abrakadabror?tab=repositories'
+API_Key = '8c51483763c4a686fcaf0c855d616909'
 
-# url = 'https://github.com/' + github_user #tworzony jest adres URL na podstawie ww. przekazanej nazwy
-response = requests.get(url) 
-if response.status_code == 200:
-    print("Everything's gonna be alright")
-elif response.status_code == 404:
-    print("Fatall error - it's a faulty operation")
-else:
-    print('Give me a patience to live in this place...an error occured!')
+city = input('enter a city: ')
 
-soup = BeautifulSoup(response.content, 'html.parser')
-# profile_image = soup.find('img', {'alt': 'Avatar'})['src']
-# print(profile_image)
+base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=' + API_Key + '&q=' + city
 
-# logo_github  = soup.find('svg', class_ =  'octicon-mark-github')
-# logo_src = logo_github.get('src')
-# print(logo_src)
-# program do wydrukowania linku do avatara uztkownika github
-
-repositories = soup.find_all(class_ = 'd-inline-block mb-1')
-repository_list = []
-for repository in repositories:
-    repository_name = repository.text.strip()
-    repository_list.append(repository_name)
-repository_dict = dict(zip(range(1, len(repository_list)+ 1), repository_list)) 
-#W powyższym kodzie, repository_dict jest słownikiem, który został utworzony za pomocą funkcji dict() i zip() z listy repository_list
-for key, value in repository_dict.items(): # W pętli for, dla każdej iteracji, key przyjmie kolejne klucze  drukujemy wyniki w jednej linii jeden pod drugim
-    print(f"Key: {key} , Value: {value}")
+weather_data = requests.get(base_url).json()
+pprint(weather_data)

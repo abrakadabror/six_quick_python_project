40e91820f253297dd1640dc59ddb7416dd49ea86
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-18 16:23:35 +0200

    changed names of files

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-unix_timestamp = 1628588167  # Przykładowy czas wschodu słońca w formacie UNIX
-utc_time = datetime.datetime.utcfromtimestamp(unix_timestamp)
-local_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
-
-print("Czas wschodu słońca (lokalny czas):", local_time)

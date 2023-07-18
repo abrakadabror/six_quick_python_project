b1c6c987cfc79b80fe8287ed11ba162fb7932f52
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-18 11:28:47 +0200

    tworzenie ramki danych pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from pprint import pprint
 
 API_Key = '8c51483763c4a686fcaf0c855d616909'
@@ -7,5 +8,23 @@ city = input('enter a city: ')
 
 base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=' + API_Key + '&q=' + city
 
-weather_data = requests.get(base_url).json()
-pprint(weather_data)
+response = requests.get(base_url)
+
+if response.status_code == 200:
+    print('good connection')
+    weather_data = response.json()
+#tworzenie slownika z danymi pogodowymi 
+    weather_dict = {
+    'City' : city,
+    'Temperature': weather_data['main']['temp'],
+    'Humidity': weather_data['main']['temp'],
+    'Weather Description': weather_data['weather'][0]['description']
+    }
+
+    df = pd.DataFrame([weather_dict])
+    print(df)
+else:
+    print('something went wrong') 
+
+
+

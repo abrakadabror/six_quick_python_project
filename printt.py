e89ec4f2427e037f7953838d064d1e7add90c33c
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-23 21:35:40 +0200

    Nested and without nested logic

--- printt.py
+++ printt.py
@@ -4,3 +4,44 @@ product = 'super_snowboard'
 
 if inventory_count > 0 or (customer_name == 'Chris' and product =='super_snowboard'):
     print(True)
+
+
+###With Nested Logic
+inventory = 0
+customer_name = 'Chris'
+if inventory <= 0:
+    if customer_name == 'Chris':
+        print('You can have the display model, sir')
+    else:
+        print('Out of stock')
+elif inventory <= 5:
+        print('Low Stock')
+else:
+        print('In stock')
+
+
+###Without Nested Logic
+inventory = 0
+customer_name = 'Chris'
+
+if customer_name == 'Chris' and inventory <= 0:
+    print('You can have the display model, sir')
+    
+elif inventory <= 0:
+        print('Out of Stock')
+elif inventory <= 5:
+        print('Low Stock')
+else:
+        print('Out of stock')
+inventory = 0
+customer_name = 'Chris'
+​
+if customer_name == 'Chris' and inventory <= 0:
+    print('You can have the display model, sir')
+    
+elif inventory <= 0:
+        print('Out of Stock')
+elif inventory <= 5:
+        print('Low Stock')
+else:
+        print('Out of stock')
